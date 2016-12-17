144afbff053443220eef278c3f7ab4f6162ce6ee
ekaterina-fedulova <ekaterina-fedulova@inbox.ru>
2016-12-17 18:15:17 +0700

    Создала ContactCreationTests
    Задание4

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactData.java
@@ -0,0 +1,31 @@
+package ru.stqa.pft.addressbook;
+
+public class ContactData {
+  private final String firstName;
+  private final String lastName;
+  private final String phone;
+  private final String mail;
+
+  public ContactData(String FirstName, String LastName, String Phone, String mail) {
+    firstName = FirstName;
+    lastName = LastName;
+    phone = Phone;
+    this.mail = mail;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public String getPhone() {
+    return phone;
+  }
+
+  public String getMail() {
+    return mail;
+  }
+}
